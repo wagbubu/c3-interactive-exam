7dbc4d152c893dfe40877f9f757b5ae7fbcbab12
Raven <raven.guirao@gmail.com>
2025-03-08 14:31:50 +0800

    Add banner

---
+++ components/Banner.tsx
@@ -0,0 +1,9 @@
+export default function Banner() {
+  return (
+    <div className="w-full bg-black py-2 bg-moss-cork text-moss-orange">
+      <p className="m-auto w-max uppercase text-xs">
+        *fast & free shipping on orders over $50!*
+      </p>
+    </div>
+  );
+}
